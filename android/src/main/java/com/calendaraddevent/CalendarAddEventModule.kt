2a5c892c570c075248d51fe400b6861e4d3d2f58
Suradis.S <suradis.workspace@gmail.com>
2024-05-31 10:27:28 +0700

    refactor: remove getter

--- android/src/main/java/com/calendaraddevent/CalendarAddEventModule.kt
+++ android/src/main/java/com/calendaraddevent/CalendarAddEventModule.kt
@@ -20,7 +20,7 @@ class CalendarAddEventModule internal constructor(context: ReactApplicationConte
   @ReactMethod
   override fun addEvent(properties: ReadableMap, promise: Promise) {
     val intent = Intent(Intent.ACTION_EDIT).setData(CalendarContract.Events.CONTENT_URI).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    val dateFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+    val dateFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     if (properties.hasKey("title")) {
       intent.putExtra(CalendarContract.Events.TITLE, properties.getString("title"))
     }
@@ -36,7 +36,7 @@ class CalendarAddEventModule internal constructor(context: ReactApplicationConte
       cEndDate.time = formattedDate
       intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, cEndDate.timeInMillis)
     }
-    val context = getReactApplicationContext()
+    val context = reactApplicationContext
     context.startActivity(intent)
     promise.resolve(properties.getString("startDate"))
   }
